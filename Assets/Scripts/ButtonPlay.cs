a88cc58a70e485b92270b6a6429b0be7711fe1ee
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2020-10-20 03:23:56 +0500

    Работа над главным меню
    Создание функции для кнопки "PLAY"

--- Assets/Scripts/ButtonPlay.cs
+++ Assets/Scripts/ButtonPlay.cs
@@ -6,11 +6,17 @@ using UnityEngine;
 public class ButtonPlay : MonoBehaviour
 {
     [SerializeField]
+    private Camera MainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void OnMouseDown()
+    {
+        MainCamera.transform.localPosition = new Vector3(0.1f, 1.903f, -8.329f);
     }
 
     // Update is called once per frame
