8e859fb344fcc7d98b04b9f8f8f04fc7778bd017
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2020-10-21 13:54:46 +0500

    Доработка кнопки "PLAY"
    Улучшен скрипт анимации при нажатии

--- Assets/Scripts/ButtonPlay.cs
+++ Assets/Scripts/ButtonPlay.cs
@@ -8,32 +8,40 @@ public class ButtonPlay : MonoBehaviour
 
     [SerializeField] private Camera MainCamera = null;
     //private GameObject button = 
-    private SpriteRenderer Button;
-
-    private SpriteRenderer AnimationButton = null;
+    private SpriteRenderer button;
     private Vector3 newScaleButton = new Vector3(1f, 1f, 1f);
+    private bool animationEditScale = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        Button = GetComponent<SpriteRenderer>();
+        button = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localScale = Vector3.Lerp(transform.localScale, newScaleButton, Time.deltaTime + 0.1f);
+        if (animationEditScale)
+        {
+            button.transform.localScale = Vector3.Lerp(transform.localScale, newScaleButton, Time.deltaTime + 0.1f);
+            if (Math.Round(button.transform.localScale.x, 2) == 0.99)
+            {
+                MainCamera.transform.localPosition = new Vector3(0.1f, 1.903f, -8.329f);
+                animationEditScale = false;
+            }
+        }
+
     }
 
     void OnMouseDown()
     {
-        newScaleButton = new Vector3(0.9f, 0.9f, 1f);
+        animationEditScale = true;
+        newScaleButton = new Vector3(0.8f, 0.8f, 1f);
     }
  
     void OnMouseUp()
     {
         newScaleButton = new Vector3(1f, 1f, 1f);
-        MainCamera.transform.localPosition = new Vector3(0.1f, 1.903f, -8.329f);
     }
 
     
