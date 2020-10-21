e204131954a47dd2ba072297611359fb209f1322
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2020-10-21 13:34:33 +0500

    Новая функция для кнопки "PLAY"
    Добавлена анимация при нажатии на кнопку.

--- Assets/Scripts/ButtonPlay.cs
+++ Assets/Scripts/ButtonPlay.cs
@@ -5,29 +5,36 @@ using UnityEngine;
 
 public class ButtonPlay : MonoBehaviour
 {
-    [SerializeField]
-    private Camera MainCamera = null;
-    //private GameObject Button = GetComponent <Sprite>
+
+    [SerializeField] private Camera MainCamera = null;
+    //private GameObject button = 
+    private SpriteRenderer Button;
+
+    private SpriteRenderer AnimationButton = null;
+    private Vector3 newScaleButton = new Vector3(1f, 1f, 1f);
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    void OnMouseDown()
-    {
-        MainCamera.transform.localPosition = new Vector3(0.1f, 1.903f, -8.329f);
+        Button = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale = Vector3.Lerp(transform.localScale, newScaleButton, Time.deltaTime + 0.1f);
     }
 
-    //void AnimationClickButton()
-    //{
-    //    while()
-    //}
+    void OnMouseDown()
+    {
+        newScaleButton = new Vector3(0.9f, 0.9f, 1f);
+    }
+ 
+    void OnMouseUp()
+    {
+        newScaleButton = new Vector3(1f, 1f, 1f);
+        MainCamera.transform.localPosition = new Vector3(0.1f, 1.903f, -8.329f);
+    }
+
+    
 }
