4d61b202d4802c712b16cdac52502f055c791a0f
Inefables <61576929+Inefables@users.noreply.github.com>
2020-10-24 17:24:32 +0500

    Добавил скрипт ButtonBack для возврата в главное меню из настроек

---
+++ Assets/Scripts/For Settings Buttons/ButtonBack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBack : MonoBehaviour
+{
+    [SerializeField] private Camera MainCamera = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseUp()
+    {
+        MainCamera.transform.localPosition = new Vector3(-6.17f, 1.903f, -8.329f);
+    }
+}
