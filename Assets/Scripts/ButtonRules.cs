5ce8ada6bf0a68d2452715d5ab2389457a43874f
Inefables <61576929+Inefables@users.noreply.github.com>
2020-10-26 17:59:27 +0500

    Добавил два скрипта: ButtonRules и ButtonClose
    Добавил слайд с правилами игры и кнопку для закрытия правил.
    ButtonRules для вызова правил игры.
    ButtonClose для возврата к настройкам игры.

---
+++ Assets/Scripts/ButtonRules.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonRules : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer settingsSprite = null;
+    [SerializeField] private SpriteRenderer rulesSprite = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseUp()
+    {
+        // Убираем со сцены слайд с настройками
+        settingsSprite.gameObject.transform.localPosition = new Vector3(0.0984f, 3.89f, -6.9f);
+
+        // Перемещаем на сцену слайд с правилами
+        rulesSprite.gameObject.transform.localPosition = new Vector3(3.93f, 1.666f, -7.658f);
+    }
+}
