6d2203104f9fefe45c5c57fbf10939cb14f4f165
Inefables <61576929+Inefables@users.noreply.github.com>
2020-10-26 17:33:19 +0500

    Добавил скрипт ButtonBackToGame
    Скрипт нужен для возобновления игры и скрытие слайда с настройками

---
+++ Assets/Scripts/ButtonBackToGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBackToGame : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer settingsSprite = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseUp()
+    {
+        Setting.pause = false;
+        settingsSprite.gameObject.transform.localPosition = new Vector3(0.0984f, 3.89f, -6.9f);
+    }
+}
