c3323944408e0280c71e6abd0f8f23d1c4dd6dec
Inefables <61576929+Inefables@users.noreply.github.com>
2020-10-26 17:24:21 +0500

    Сделал заготовку для кнопки Вкл/Выкл Вибрации
    Фунцию включения и выключения вибрации добавим позже, когда вибрация будет активирована при ошибочном выборе стаканчика

---
+++ Assets/Scripts/ButtonVibration.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonVibration : MonoBehaviour
+{
+    private SpriteRenderer button;
+    [SerializeField] private Sprite spriteVibrationOn = null;
+    [SerializeField] private Sprite spriteVibrationOff = null;
+
+    // Включена ли вибрация в игре
+    private bool isTheVibrationTurnedOn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseUp()
+    {
+        if (isTheVibrationTurnedOn)
+        {
+            // Выключаем вибрации в игре
+            
+            // Меняем картинку у кнопки на "button_vibrateOFF"
+            button.sprite = spriteVibrationOff;
+
+            isTheVibrationTurnedOn = false;
+        }
+        else
+        {
+            // Включаем вибрации в игре
+            
+            // Меняем картинку у кнопки на "button_vibrateON"
+            button.sprite = spriteVibrationOn;
+
+            isTheVibrationTurnedOn = true;
+        }
+
+    }
+}
