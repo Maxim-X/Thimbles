12266510e6b9da3ee5f46b5e88e2dd7a3b7b6fbb
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2020-10-31 20:38:24 +0500

    Изменил расположение шарика под стаканчиком

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -89,7 +89,7 @@ public class Game : MonoBehaviour
                         //Cup_2.transform.localPosition = Setting.def_position_cup_2;
                         //Cup_3.transform.localPosition = Setting.def_position_cup_3;
                         correct_cup = AllCup[UnityEngine.Random.Range(0, 3)];
-                        ball.transform.localPosition = correct_cup.transform.localPosition;
+                        ball.transform.localPosition = new Vector3(correct_cup.transform.localPosition.x, correct_cup.transform.localPosition.y - 0.2f, correct_cup.transform.localPosition.z);
                         coord_correct_cup_raise = new Vector3(correct_cup.transform.localPosition.x, correct_cup.transform.localPosition.y + 0.5f, correct_cup.transform.localPosition.z);
                         coord_correct_cup_def = correct_cup.transform.localPosition;
                         timer_start = 0;
@@ -183,7 +183,7 @@ public class Game : MonoBehaviour
                         coord_correct_cup_raise = new Vector3(choiceCup.transform.localPosition.x, choiceCup.transform.localPosition.y + 0.5f, choiceCup.transform.localPosition.z);
                         coord_correct_cup_def = choiceCup.transform.localPosition;
                         correct_cup_y_pre = 1;
-                        ball.transform.localPosition = correct_cup.transform.localPosition;
+                        ball.transform.localPosition = new Vector3(correct_cup.transform.localPosition.x, correct_cup.transform.localPosition.y - 0.2f, correct_cup.transform.localPosition.z);
                     }
                     else // поднимаем правильный стаканчик
                     {
