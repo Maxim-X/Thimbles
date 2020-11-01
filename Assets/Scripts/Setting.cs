71d487bec4eaf0aca635e7829e556f3989dec828
Inefables <61576929+Inefables@users.noreply.github.com>
2020-11-02 03:23:51 +0500

    Сделали рекорды
    Аллаху Акбар

--- Assets/Scripts/Setting.cs
+++ Assets/Scripts/Setting.cs
@@ -49,6 +49,12 @@ public class Setting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Обнуление списка рекордов единожды перед сборкой проекта
+        //добавим сюда ещё очистку макс рекорда
+        //PlayerPrefs.SetString("RecordsList", recordText);
+        //max_record = 0;
+        //PlayerPrefs.SetString("maxrecord", max_record.ToString());
+
         def_position_cup_1 = cup_1.transform.localPosition;
         def_position_cup_2 = cup_2.transform.localPosition;
         def_position_cup_3 = cup_3.transform.localPosition;
@@ -57,6 +63,7 @@ public class Setting : MonoBehaviour
         pointsAtGameOverSprite_TextMeshPro = GameObject.FindWithTag("Points").GetComponent<TextMeshPro>();
         stat = GameObject.FindWithTag("Stats").GetComponent<TextMeshPro>();
         recordsNumbersSprite_TextMeshPro = GameObject.FindWithTag("RecordsList").GetComponent<TextMeshPro>();
+        max_record = PlayerPrefs.GetInt("maxrecord");
         stat.text = max_record.ToString();
 
         // Обнуление списка рекордов единожды перед сборкой проекта
